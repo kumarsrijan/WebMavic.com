6276ae2fd5ab68cacad84e8761d9e8e031660dd1
Rakshit <rakshit.dubey2002@gmail.com>
2024-01-20 01:03:46 +0530

    Init

---
+++ src/components/choose/WhatWeDo5.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+function WhatWeDo5() {
+  return (
+    <div className="home5-process-area mb-130">
+      <div className="container">
+        <div
+          className="row mb-55 wow animate fadeInUp"
+          data-wow-delay="200ms"
+          data-wow-duration="1500ms"
+        >
+          <div className="col-lg-12 d-flex align-items-center justify-content-between flex-wrap gap-2">
+            <div className="section-title-5">
+              <span>THE PROCESS</span>
+              <h2>What We Do</h2>
+            </div>
+            {/* <div className="section-content">
+            <p>Morbi interdum pellentesque nunc, luctusongi pretium nulla luctus at Aliquam. </p>
+          </div> */}
+          </div>
+        </div>
+        <div className="row g-lg-4 gy-5 justify-content-center">
+          <div
+            className="col-lg-4 col-sm-6 wow animate fadeInUp"
+            data-wow-delay="200ms"
+            data-wow-duration="1500ms"
+          >
+            <div className="single-process">
+              <div className="sl">
+                <h2>01</h2>
+              </div>
+              <div className="content">
+                <h3>Research &amp; Discovery</h3>
+                <p>
+                  Achieving optimal results involves delving deep into a
+                  comprehensive understanding of both the specific requirements
+                  and the anticipated outcomes associated with each task.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-lg-4 col-sm-6 wow animate fadeInUp"
+            data-wow-delay="300ms"
+            data-wow-duration="1500ms"
+          >
+            <div className="single-process">
+              <div className="sl">
+                <h2>02</h2>
+              </div>
+              <div className="content">
+                <h3>Industry Expertise</h3>
+                <p>
+                  As individuals living in the 21st century, we instinctively
+                  favor the technologies that characterize our time. This
+                  inclination is a deliberate effort to keep our products
+                  current and adaptable, ensuring their continued relevance and
+                  security in the decades to come.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-lg-4 col-sm-6 wow animate fadeInUp"
+            data-wow-delay="400ms"
+            data-wow-duration="1500ms"
+          >
+            <div className="single-process">
+              <div className="sl">
+                <h2>03</h2>
+              </div>
+              <div className="content">
+                <h3>Quality Assurance</h3>
+                <p>
+                  Hailing from an engineering background at BIT Mesra, our
+                  youthful energy and enthusiasm uniquely position us. This
+                  background not only instills a strong work ethic but also
+                  drives us to dedicate extra time and effort, surpassing
+                  established benchmarks. With this combination, we aim not just
+                  to meet but to exceed expectations and achieve goals that set
+                  a higher standard.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="achievement-area">
+          <div className="row g-lg-4 gy-5">
+            <div
+              className="col-xl-7 col-lg-6 d-flex align-items-center wow animate fadeInLeft"
+              data-wow-delay="200ms"
+              data-wow-duration="1500ms"
+            >
+              <div className="achievement-content">
+                <h2>#1</h2>
+                <h3>
+                  One Stop solutons for your business needs{" "}
+                  <span>
+                    By engineers ofBIT <span className="year">BIT.</span>
+                  </span>
+                </h3>
+              </div>
+            </div>
+            <div
+              className="col-xl-5 col-lg-6 wow animate fadeInRight"
+              data-wow-delay="300ms"
+              data-wow-duration="1500ms"
+            >
+              <div className="achievement-img magnetic-item">
+                <img
+                  className="img-fluid"
+                  src="assets/img/home-5/achievement-img.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          {/* <div className="achievement-text-slider ">
+            <Marquee>
+              {" "}
+              <h2 className="marquee_text2">
+                Learn<span>#</span>useful information<span>#</span>from our
+                insights<span>#</span>and<span>#</span>experience<span>#</span>
+                together ani <span>#</span>Learn<span>#</span>useful information
+                <span>#</span>from our insights<span>#</span>and<span>#</span>
+                experience<span>#</span>together ani<span>#</span>Learn
+                <span>#</span>useful information<span>#</span>from our insights
+                <span>#</span>and<span>#</span>experience<span>#</span>together
+                ani<span>#</span>Learn<span>#</span>useful information
+                <span>#</span>from our insights<span>#</span>and<span>#</span>
+                experience<span>#</span>together ani
+              </h2>{" "}
+            </Marquee>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WhatWeDo5;
