093b95ae8d5d9690ff8268d706e124b6778e25b1
Rakshit <rakshit.dubey2002@gmail.com>
2024-02-08 22:02:11 +0530

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,35 @@
 import Breadcrumb from "@/components/common/Breadcrumb";
 import Layout from "@/components/layout/Layout";
-import React from "react";
+import { newContact } from "@/utils/appwrite";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function Contactpage() {
+
+  const [data, setData] = useState({
+    name: null,
+    whatsapp: null,
+    cname: null,
+    gst: null,
+    email: null,
+    phone: null,
+    message: null,
+    services: null,
+    service: null
+  })
+  const [disable, setDisable] = useState(false)
+  const router = useRouter()
+
+  const contact = async (e) => {
+    e.preventDefault()
+    setDisable(true)
+    const { status, message } = await newContact(data)
+    alert(message)
+    if (status) router.refresh()
+    setDisable(false)
+  }
+
+
   return (
     <Layout>
       <Breadcrumb
@@ -91,24 +118,24 @@ function Contactpage() {
                   <h5>Make a Free Consulting</h5>
                 </div>
                 <div className="contact-form">
-                  <form>
+                  <form onSubmit={contact} >
                     <div className="row">
                       <div className="col-md-6 mb-20">
                         <div className="form-inner">
                           <label> name :</label>
-                          <input type="text" />
+                          <input value={data.name} onChange={e => setData({ ...data, name: e.target.value })} type="text" />
                         </div>
                       </div>
                       <div className="col-md-6 mb-20">
                         <div className="form-inner">
                           <label>WhatsApp Number:</label>
-                          <input type="number" />
+                          <input value={data.whatsapp} onChange={e => setData({ ...data, whatsapp: e.target.value })} type="number" />
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Company Name :</label>
-                          <input type="text" />
+                          <input value={data.cname} onChange={e => setData({ ...data, cname: e.target.value })} type="text" />
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
@@ -121,6 +148,7 @@ function Contactpage() {
                               name="gst-check"
                               type="radio"
                               value="Yes"
+                              onClick={e => setData({ ...data, gst: true })}
                               id="gst-yes"
                               style={{ display: "none" }}
                             />
@@ -138,6 +166,7 @@ function Contactpage() {
                               name="gst-check"
                               type="radio"
                               value="No"
+                              onClick={e => setData({ ...data, gst: false })}
                               id="gst-no"
                               style={{ display: "none" }}
                             />
@@ -157,31 +186,46 @@ function Contactpage() {
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Email</label>
-                          <input type="email" />
+                          <input value={data.email} onChange={e => setData({ ...data, email: e.target.value })} type="email" />
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Phone</label>
-                          <input type="number" />
+                          <input value={data.phone} onChange={e => setData({ ...data, phone: e.target.value })} type="number" />
                         </div>
                       </div>
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Services You Need ?</label>
-                          <input type="number" />
+                          <select onChange={e => setData({ ...data, services: e.target.value })}>
+                            <option value="" disabled selected>Select your option</option>
+                            <option value={"Web design"}>Web Design</option>
+                            <option value={"Web Development"}>Web Development</option>
+                            <option value={"App development"}>App Development</option>
+                            <option value={"Graphic design"}>Graphic Design</option>
+                            <option value={"Copywritting"}>Copy Writting</option>
+                            <option value={"SEO"}>SEO</option>
+                            <option value={"Other"}>Other</option>
+                          </select>
                         </div>
                       </div>
+                      {data.services === "Other" && <div className="col-lg-12 mb-20">
+                        <div className="form-inner">
+                          <label>Other Service Like</label>
+                          <input value={data.service} onChange={e => setData({ ...data, service: e.target.value })} type="text" />
+                        </div>
+                      </div>}
                       <div className="col-lg-12 mb-20">
                         <div className="form-inner">
                           <label>Message</label>
-                          <textarea defaultValue={""} />
+                          <textarea value={data.message} onChange={e => setData({ ...data, message: e.target.value })} />
                         </div>
                       </div>
                       <div className="col-lg-12">
                         <div className="form-inner">
-                          <button className="primary-btn3" type="submit">
-                            Submit
+                          <button disabled={disable} className="primary-btn3" type="submit">
+                            {disable ? "Loading..." : "Submit"}
                           </button>
                         </div>
                       </div>
@@ -192,8 +236,8 @@ function Contactpage() {
             </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </div >
+    </Layout >
   );
 }
 
---
+++ src/utils/appwrite.js
@@ -0,0 +1,42 @@
+import { Client, Databases, ID } from 'appwrite';
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('65c4dfed3f07844a098d');
+
+const db = new Databases(client)
+
+export async function newContact(data) {
+    console.log("Reaching");
+    try {
+
+        if (!data.name) throw { status: 404, message: "Need a Name to proceed" }
+        if (!data.email) throw { status: 404, message: "Need an Email to proceed" }
+        if (!data.phone) throw { status: 404, message: "Need Phone Number to proceed" }
+
+        const { $id } = await db.createDocument(
+            "contact",
+            "people",
+            ID.unique(),
+            {
+                name: data.name,
+                whatsapp: data.whatsapp,
+                cname: data.cname,
+                gst: data.gst,
+                email: data.email,
+                phone: data.phone,
+                message: data.message,
+                services: data.services,
+                service: data.service
+            }
+        );
+
+        if (!$id) throw { message: "Not Uploaded" }
+
+        return { status: true, message: "Uploaded Successfully" }
+
+    } catch (error) {
+        console.log(error);
+        return { status: false, message: error.message || "Something went wrong" }
+    }
+}
