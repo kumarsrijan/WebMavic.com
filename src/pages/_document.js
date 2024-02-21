5674988900da95eec89796a70130cc47aa821ae1
Kumar Srijan <49157668+kumarsrijan@users.noreply.github.com>
2024-02-21 17:05:24 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        rel="icon"
+        type="image/x-icon"
+        href="assets/img/webmavic-icon.png"
+      />
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
